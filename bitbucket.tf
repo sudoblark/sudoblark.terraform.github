d2173011e23cb8b47f950df6c1f4f91c83bd309b
Benjamin Clark <bclark@sudoblark.com>
2024-09-24 09:59:43 +0100

    Initial creation of bitbucket cli repo

---
+++ bitbucket.tf
@@ -0,0 +1,9 @@
+locals {
+  bitbucket = {
+    "sudoblark.bitbucket.cli" : {
+      description : "A CLI application created to manage BitBucket resources.",
+      topics : ["python", "bitbucket", "cli"],
+      open_source : true,
+    },
+  }
+}
