2ef5a17cae40840e51a9f08040ac8759bdc84fc8
Benjamin Clark <bclark@sudoblark.com>
2024-09-26 11:51:08 +0100

    Mark sudoblark.terrraform.github as public

--- core-platform.tf
+++ core-platform.tf
@@ -2,7 +2,8 @@ locals {
   core_platform = {
     "sudoblark.terraform.github" : {
       description : "Terraform setup for Sudoblark GitHub",
-      topics : ["terraform", "github", "iac"]
+      topics : ["terraform", "github", "iac"],
+      visibility : "public"
     },
     "sudoblark.terraform.aws" : {
       description : "Terraform setup for Sudoblark AWS",
