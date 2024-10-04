c84e8f6c8671fed912e11b2714918357156d3a34
Benjamin Clark <bclark@sudoblark.com>
2024-10-04 12:15:05 +0100

    Create terraform demo repo for conference

--- python.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  python = {
-    "sudoblark.python.bitbucket-cli" : {
-      description : "A CLI application created to manage BitBucket resources.",
-      topics : ["python", "bitbucket", "cli"],
-      open_source : true,
-    },
-  }
-}
