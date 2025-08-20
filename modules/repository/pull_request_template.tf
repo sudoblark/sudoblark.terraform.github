02918e8f9d99d3794777ee308889adb16565dc0f
Benjamin Clark <bclark@sudoblark.com>
2025-08-20 11:27:13 +0100

    Simplify pull_request_template

---
+++ modules/repository/pull_request_template.tf
@@ -0,0 +1,24 @@
+locals {
+  is_python_repo    = can(regex("python", github_repository.repository.name))
+  is_terraform_repo = can(regex("terraform", github_repository.repository.name))
+}
+
+# github_repository_file resources are created sequentially to preserve linear commit history
+resource "github_repository_file" "pull_request_template" {
+  repository = github_repository.repository.name
+  branch     = data.github_branch.main.branch
+  file       = ".github/pull_request_template.md"
+  content = templatefile("${path.module}/template_files/pull_request_template.tftpl", {
+    PYTHON_REPO    = local.is_python_repo
+    TERRAFORM_REPO = local.is_terraform_repo
+  })
+  commit_message      = format(".github/pull_request_template.md- managed by sudoblark.terraform.github")
+  overwrite_on_create = true
+
+  depends_on = [
+    data.github_branch.main,
+    # Done to preserve linear commit history
+    github_repository_file.codeowners,
+    github_repository_file.license
+  ]
+}
